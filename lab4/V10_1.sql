68fcb6c8e2c6c66733f60566916ed2fa06ecb2a3
IvanNarkevich <47961410+IvanNarkevich@users.noreply.github.com>
2020-10-31 23:38:32 +0300

    Lab4

---
+++ lab4/V10_1.sql
@@ -0,0 +1,118 @@
+﻿USE AdventureWorks2012;
+
+/*
+	Создайте таблицу Sales.SalesReasonHst, которая будет хранить информацию об изменениях в таблице Sales.SalesReason.
+	Обязательные поля, которые должны присутствовать в таблице: 
+	ID — первичный ключ IDENTITY(1,1); 
+	Action — совершенное действие (insert, update или delete); 
+	ModifiedDate — дата и время, когда была совершена операция; 
+	SourceID — первичный ключ исходной таблицы; 
+	UserName — имя пользователя, совершившего операцию. 
+	Создайте другие поля, если считаете их нужными.
+*/
+
+CREATE TABLE Sales.SalesReasonHst (
+    ID INT IDENTITY(1,1) PRIMARY KEY,
+    Action NVARCHAR(20) NOT NULL,
+    ModifiedDate DATETIME NOT NULL,
+    SourceID INT NOT NULL,
+    UserName NVARCHAR(120) NOT NULL
+);
+GO
+
+/*
+	 Создайте три AFTER триггера для трех операций INSERT, UPDATE, DELETE для таблицы Sales.SalesReason. 
+	 Каждый триггер должен заполнять таблицу Sales.SalesReasonHst с указанием типа операции в поле Action.
+*/
+
+CREATE TRIGGER TRG_SalesReason_AfterInsert
+ON Sales.SalesReason
+AFTER INSERT AS
+BEGIN
+    INSERT INTO Sales.SalesReasonHst (
+	        Action,
+			ModifiedDate,
+			SourceID,
+			UserName)
+    SELECT 
+			'INSERT', 
+			CURRENT_TIMESTAMP, 
+			SalesReasonID, 
+			CURRENT_USER
+    FROM inserted;
+END
+GO
+
+CREATE TRIGGER TRG_SalesReason_AfterUpdate
+ON Sales.SalesReason
+AFTER UPDATE AS
+BEGIN
+    INSERT INTO Sales.SalesReasonHst (
+	        Action,
+			ModifiedDate,
+			SourceID,
+			UserName)
+    SELECT
+            'UPDATE', 
+			CURRENT_TIMESTAMP, 
+			SalesReasonID, 
+			CURRENT_USER
+	FROM inserted;
+END
+GO
+
+CREATE TRIGGER TRG_SalesReason_AfterDelete
+ON Sales.SalesReason
+AFTER DELETE AS
+BEGIN
+    INSERT INTO Sales.SalesReasonHst (
+            Action,
+			ModifiedDate,
+			SourceID,
+			UserName)
+    SELECT 
+    	    'DELETE', 
+			CURRENT_TIMESTAMP, 
+			SalesReasonID, 
+			CURRENT_USER
+	FROM deleted;
+END
+GO
+
+/*
+	 Создайте представление VIEW, отображающее все поля таблицы Sales.SalesReason. 
+	 Сделайте невозможным просмотр исходного кода представления.
+*/
+
+CREATE VIEW Sales.vSalesReason 
+WITH ENCRYPTION AS
+	SELECT * 
+	FROM Sales.SalesReason;
+GO
+
+/*
+	Вставьте новую строку в Sales.SalesReason через представление. Обновите вставленную строку. 
+	Удалите вставленную строку. Убедитесь, что все три операции отображены в Sales.SalesReasonHst.
+*/
+
+INSERT INTO Sales.vSalesReason (
+	Name, 
+	ReasonType, 
+	ModifiedDate)
+VALUES (
+	'MyName', 
+	'Other', 
+	CURRENT_TIMESTAMP);
+GO
+
+UPDATE Sales.vSalesReason 
+SET Name = 'Name'
+WHERE Name = 'MyName';
+GO
+
+DELETE FROM Sales.vSalesReason
+WHERE Name = 'Name';
+GO
+
+SELECT *
+FROM Sales.SalesReasonHst;
---
+++ lab4/V10_2.sql
@@ -0,0 +1,121 @@
+﻿USE AdventureWorks2012;
+GO
+
+/*
+	Создайте представление VIEW, отображающее данные из таблиц Sales.SalesReason и Sales.SalesOrderHeaderSalesReason, 
+	а также CustomerID из таблицы Sales.SalesOrderHeader. Создайте уникальный кластерный индекс в представлении по полям SalesReasonID, SalesOrderID.
+*/
+
+CREATE VIEW Sales.vSalesOrderReason
+WITH SCHEMABINDING AS
+SELECT 
+    SOHSR.SalesOrderID, 
+	SOHSR.SalesReasonID, 
+	SR.Name, 
+	SR.ReasonType, 
+	SOH.CustomerID
+FROM Sales.SalesOrderHeaderSalesReason AS SOHSR
+INNER JOIN Sales.SalesReason AS SR
+    ON (SR.SalesReasonID = SOHSR.SalesReasonID)
+INNER JOIN Sales.SalesOrderHeader AS SOH
+    ON (SOH.SalesOrderID = SOHSR.SalesOrderID);
+GO
+
+CREATE UNIQUE CLUSTERED INDEX UCIndex_vSalesOrderReason
+	ON Sales.vSalesOrderReason (SalesReasonID, SalesOrderID);
+GO
+
+/*
+	Создайте один INSTEAD OF триггер для представления на три операции INSERT, UPDATE, DELETE. 
+	Триггер должен выполнять соответствующие операции в таблицах Sales.SalesReason и Sales.SalesOrderHeaderSalesReason 
+	для указанного CustomerID. Обновление не должно происходить в таблице Sales.SalesOrderHeaderSalesReason. 
+	Удаление из таблицы Sales.SalesReason производите только в том случае, если удаляемые строки больше не ссылаются на Sales.SalesOrderHeaderSalesReason.
+*/
+
+CREATE TRIGGER TRG_vSalesOrderReason
+ON Sales.vSalesOrderReason
+INSTEAD OF UPDATE, INSERT, DELETE
+AS
+BEGIN
+	IF EXISTS(SELECT * FROM inserted) AND EXISTS (SELECT * FROM deleted)
+	BEGIN
+		UPDATE Sales.SalesReason
+		SET
+		Name = I.Name,
+		ReasonType = I.ReasonType,
+		ModifiedDate = GETDATE()
+		FROM Sales.SalesReason AS SR
+		INNER JOIN inserted AS I
+			ON I.SalesReasonId = SR.SalesReasonId    
+	END
+
+	ELSE IF EXISTS(SELECT * FROM inserted)
+	BEGIN
+		IF NOT EXISTS(
+			SELECT * FROM Sales.SalesReason AS SR
+			JOIN inserted ON inserted.SalesReasonID = SR.SalesReasonID
+			WHERE SR.SalesReasonID = inserted.SalesReasonID)
+		BEGIN
+			INSERT INTO Sales.SalesReason (Name, ReasonType, ModifiedDate)
+			SELECT Name, ReasonType, GETDATE()
+			FROM inserted
+		END
+		ELSE
+		BEGIN
+			UPDATE Sales.SalesReason
+			SET
+			Name = I.Name,
+			ReasonType = I.ReasonType,
+			ModifiedDate = GETDATE()
+			FROM inserted AS I
+			WHERE Sales.SalesReason.SalesReasonId = I.SalesReasonId
+		END
+		INSERT INTO Sales.SalesOrderHeaderSalesReason (SalesOrderId, SalesReasonID, ModifiedDate)
+		SELECT I.SalesOrderID, I.SalesReasonID, GETDATE()
+		FROM inserted AS I
+	END
+
+	ELSE IF EXISTS (SELECT * FROM deleted)
+	BEGIN
+		DELETE FROM Sales.SalesOrderHeaderSalesReason
+		WHERE SalesOrderID IN (SELECT SalesOrderID FROM deleted)
+			AND SalesReasonID IN (SELECT SalesReasonId FROM deleted)
+
+		DELETE FROM Sales.SalesReason 
+		WHERE SalesReasonId = (SELECT SalesReasonId FROM deleted)
+			AND SalesReasonId NOT IN (SELECT SalesReasonId FROM Sales.SalesOrderHeaderSalesReason)
+	END;
+END;
+GO
+
+/*
+	Вставьте новую строку в представление, указав новые данные SalesReason для существующего CustomerID (например для 11000). 
+	Триггер должен добавить новые строки в таблицы Sales.SalesReason и Sales.SalesOrderHeaderSalesReason. 
+	Обновите вставленные строки через представление. Удалите строки.
+*/
+
+INSERT INTO Sales.vSalesOrderReason (
+	SalesOrderID, 
+	SalesReasonID, 
+	Name, 
+	ReasonType, 
+	CustomerID)
+VALUES (
+	57418, 
+	6, 
+	'New name', 
+	'New Type', 
+	11000);
+
+
+UPDATE Sales.vSalesOrderReason 
+SET Name = 'Review', 
+	ReasonType = 'Other'
+WHERE SalesOrderID = 57418
+    AND SalesReasonID = 6
+	AND CustomerID = 11000
+
+DELETE FROM Sales.vSalesOrderReason 
+WHERE SalesOrderID = 57418
+    AND SalesReasonID = 6
+	AND CustomerID = 11000
